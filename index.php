cbb01c583daaeabca6df3409ee9fa4b692ea75ac
Raju Shrestha <rshrestha9@mail.greenriver.edu>
2021-05-11 12:06:18 -0400

    index

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
+//Start session start
+session_start();
+
+
 //require autoload file
 require_once('vendor/autoload.php');
 
@@ -13,17 +17,41 @@ $f3=Base::instance();
 $f3->route('GET /',function (){
    //Display the home page
     $view = new Template();
-    echo $view-> render('views/home.html');
+    echo $view-> render('views/info.html');
 });
 
-$f3->route('GET /Orderform',function (){
+$f3->route('GET|POST /Order1',function (){
+    //if the form has submitted , add the  data to session and send the user to the next order form
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $_SESSION['food']=$_POST['food'];
+        $_SESSION['meal']=$_POST['meal'];
+        header('location: order2');
+    }
     //Display the home page
     $view = new Template();
-    echo $view-> render('views/Orderform.html');
+    echo $view-> render('views/orderForm1.html');
+});
+
+$f3->route('GET|POST /Order2',function (){
+
+    //if the form has submitted , add the  data to session and send the user to the next order form
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        //Data val wiil go here
+
+        $_SESSION['conds']=$_POST['conds'];
+        header('location: summary');
+    }
+    //Display the home page
+    $view = new Template();
+    echo $view-> render('views/orderForm2.html');
+});
+
+$f3-> route('GET /summary',function (){
+   $view = new Template();
+   echo $view-> render('views/summary.html');
 });
 
 
-//$f3-> route('GET /Orderform.html')
 
 //Run Fat-Free
 $f3->run();
